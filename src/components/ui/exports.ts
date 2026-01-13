7578246ad34dd8007f2d89aea5daf3a39a5308b8
afernandez-art <afernandez@gallanoworldwide.com>
2026-01-13 14:05:51 -0300

    feat: Add UI components exports

---
+++ src/components/ui/exports.ts
@@ -0,0 +1,3 @@
+export { KPICard } from "./KPICard";
+export { StatusBadge, StatusDot } from "./StatusBadge";
+export type { Status, InboundStatus, StockStatus, OutboundStatus } from "./StatusBadge";
